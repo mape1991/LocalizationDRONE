8bb77e02da30b3accd8bca84d8e486b2c338461f
aesparre <aesparre@ubuntu.(none)>
2013-11-27 12:45:07 +0100

    UDP update test du drone avec new function (a tester)

--- ardrone_indoor_commons/com/udp_comm.h
+++ ardrone_indoor_commons/com/udp_comm.h
@@ -22,8 +22,7 @@
 typedef void (*On_received_callback)(char *);  
 
 int udp_open_socket();
-int udp_listen(int lg_mesg_emis,int port); /* listen to drone, return a type of errors */
-int udp_listen_once(char *message, int lg_mesg_emis, int port);
+int udp_listen_once(char *message, int lg_mesg_emis, int port);  /* listen to drone, return a type of errors */
 int udp_send(char * dest, char *message, int size, int port); /*  */
 int udp_send_char(char * dest, char message, int port);
 int udp_respond(char* message, int size, int port);
--- ardrone_indoor_drone/src/tests/test_comm.c
+++ ardrone_indoor_drone/src/tests/test_comm.c
@@ -10,26 +10,26 @@ void test_comm_main()
    char message_sent_id;
 
    printf("demo program launched\n\n");
-   udp_listen_once(message, COMM_MESSAGE_SIZE, PORT_SERVER_TO_DRONE);
-   while (1){
+   udp_open_socket();
+   
+   do {
+      udp_listen_once(message, COMM_MESSAGE_SIZE, PORT_SERVER_TO_DRONE);
       if (message[0] == 'I'){
          printf("message init received\n");
          strcpy(message_sent_id, UDP_MESSAGE_DRONE_INIT_ID);
-         udp_send(DEST_IP, message_sent_id, UDP_MESSAGE_DRONE_SIZE, PORT_DRONE_TO_SERVER);
-         udp_listen_once(message, COMM_MESSAGE_SIZE, PORT_SERVER_TO_DRONE);
+         udp_respond(message_sent_id, UDP_MESSAGE_DRONE_SIZE, PORT_DRONE_TO_SERVER);
       }
       else if (message[0] == 'S'){
          printf("message sync received\n");
          strcpy(message_sent_id, UDP_MESSAGE_DRONE_SYNC_ID);
-         udp_send(DEST_IP, message_sent_id, UDP_MESSAGE_DRONE_SIZE, PORT_DRONE_TO_SERVER);
-         udp_listen_once(message, COMM_MESSAGE_SIZE, PORT_SERVER_TO_DRONE);
-
+         udp_respond(message_sent_id, UDP_MESSAGE_DRONE_SIZE, PORT_DRONE_TO_SERVER);
       }
       else if (message[0] == 'X'){
-               printf("message quit received\n");
-               break;
+         printf("message quit received\n");
+         break;
       }
-   }
+   while (1);
+   udp_close_socket();
 }
 
 #endif
