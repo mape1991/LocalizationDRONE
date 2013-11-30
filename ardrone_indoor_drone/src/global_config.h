c1763dd19aaba7c594011af0204eefdcb0d1fc3f
Julien <julien@ubuntu.(none)>
2013-11-30 09:21:31 -0800

    drone tests fix for one-character messages

--- ardrone_indoor_drone/src/global_config.h
+++ ardrone_indoor_drone/src/global_config.h
@@ -15,13 +15,13 @@
  * i.e. if GUI_ON is commented, nothing related to user interface will be activated 
  */
 // debug tracking
-//#define DEBUG_ON
+#define DEBUG_ON
 // udp communication
 //#define UDP_ON
 
 // Tests
-//#define TEST_COMM             // test comm on
-#define TEST_WIFI_DELAY
+#define TEST_COMM             // test comm on
+//#define TEST_WIFI_DELAY
 
 #ifdef TEST_COMM
     #define UDP_ON
