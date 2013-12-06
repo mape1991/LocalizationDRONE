6b800709e77d06ed8db694f61a3319fe074adc2e
aesparre <aesparre@etud.insa-toulouse.fr>
2013-12-06 14:38:14 +0100

    removed unnecessary includes

--- ardrone_indoor_drone/src/tests/test_thread.c
+++ ardrone_indoor_drone/src/tests/test_thread.c
@@ -1,16 +1,9 @@
 #include "test_full.h"
 
-#include <stdlib.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <termios.h>
