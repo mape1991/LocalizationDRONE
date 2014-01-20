f79fe08a91aa2f4e7f33af0708ea91b211bd7f72
Nguyen Vu Hai <julien@ubuntu.(none)>
2014-01-20 07:15:26 -0800

    set drone in test_full mode

--- ardrone_indoor_drone/src/global_config.h
+++ ardrone_indoor_drone/src/global_config.h
@@ -22,10 +22,10 @@
 // Tests
 //#define TEST_COMM             // test comm on
 //#define TEST_WIFI_DELAY
-//#define TEST_FULL
+#define TEST_FULL
 //#define TEST_USB
 /** test_thread code is nested in the test_full code */
-#define TEST_THREAD
+//#define TEST_THREAD
 
 #ifdef TEST_COMM
    #define UDP_ON
